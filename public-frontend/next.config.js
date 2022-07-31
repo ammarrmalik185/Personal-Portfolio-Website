ca0bcbc6752e9608da4be50c39069f3b74490fc4
Ammar Malik <ammarrmalik185@hotmail.com>
2022-07-31 22:08:01 +0500

    Added configurations

--- public-frontend/next.config.js
+++ public-frontend/next.config.js
@@ -1,6 +1,12 @@
+require("dotenv")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "./build/" + process.env.NODE_ENV + "/v" + process.env.VERSION,
+  images: {
+    loader: 'akamai',
+    path: '/images'
+  }
 }
 
 module.exports = nextConfig
